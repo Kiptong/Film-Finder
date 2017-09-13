2d7bcc059d26099af68ad19123bd994c0b9459a9
Kip Gibson <Kiptong@gmail.com>
2017-09-12 23:59:35 -0700

    Adding omdb-client. #1

--- server/server.js
+++ server/server.js
@@ -1,4 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const imdb = require('imdb-api')
+
 const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/search' , (req, res) => {
+  const film = req.body
+
+  
+})
 
 app.listen(3000, () => console.log('Listening on 3000!'))
