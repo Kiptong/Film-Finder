5630c6585e406e68c24318d0d9899fe755071476
Kip Gibson <Kiptong@gmail.com>
2017-09-13 10:10:29 -0700

    Removing omdb-clien, Adding omdb. #1

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const imdb = require('imdb-api')
+const omdb = require('omdb')
 
 const app = express()
 
 app.use(bodyParser.json())
 
 app.post('/search' , (req, res) => {
-  const film = req.body
-
-  
 })
 
 app.listen(3000, () => console.log('Listening on 3000!'))
