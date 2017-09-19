f920ad378bef3852a2891774fd262bbc369b4ccc
Kip Gibson <Kiptong@gmail.com>
2017-09-18 23:29:11 -0700

    Adding HTML structure to search-bar component. #1

--- components/search-bar.js
+++ components/search-bar.js
@@ -5,4 +5,25 @@ export default class Search extends Component {
     super(props)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  render() {
+    return (
+      <div className="container">
+	      <div className="row">
+          <div className="col-sm-6 col-sm-offset-3">
+            <div id="imaginary_container">
+              <div className="input-group stylish-input-group">
+                <input type="text" className="form-control"  placeholder="Search" >
+                <span className="input-group-addon">
+                  <button type="submit">
+                    <span className="glyphicon glyphicon-search"></span>
+                  </button>
+                </span>
+              </div>
+            </div>
+          </div>
+	      </div>
+      </div>
+    )
+  }
 }
